d8d2cfaf9bee7dd3bcf9de60f9f8b6544ad53b90
Daniel Ilett <danielilett@gmail.com>
2020-10-25 21:27:39 +0000

    Additions ready for public release.

---
+++ Assets/Scripts/Materials.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Materials : MonoBehaviour
+{
+    public MeshRenderer rend;
+    public List<Material> materials;
+    int index = 0;
+
+    void Start()
+    {
+        InvokeRepeating("Swap", 2.0f, 4.0f);
+    }
+
+    private void Swap()
+    {
+        index++;
+
+        if(index == materials.Count)
+        {
+            index = 0;
+        }
+
+        rend.material = materials[index];
+    }
+}
